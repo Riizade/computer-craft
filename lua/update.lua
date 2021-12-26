ad55c853a919a9b00a6cc62c8064ad52bed24446
Adam Johnson <riizade@gmail.com>
2021-12-25 21:49:12 -0800

    update update again

--- lua/update.lua
+++ lua/update.lua
@@ -6,6 +6,7 @@ args = {...}
 first_arg = args[1]
 
 function download_from_github(filename)
+    print("updating " .. filename .. "...")
     local response = http.get("https://raw.githubusercontent.com/Riizade/computer-craft/main/lua/" .. filename .. ".lua")
     shell.run("delete", filename) --- clear the old version
     local file = fs.open(filename, "w") --- open and write file
@@ -21,12 +22,10 @@ end
 
 --- if "ready" is passed as the first argument, update all other scripts ---
 if first_arg == "ready" then
-    print("updating all scripts...")
     files = {"mine", "strip_mine", "turtle_min_fuel", "fell2", "floor", "items"}
     update_files(files)
 else --- otherwise only update the update script, then call it to update other scripts ---
     files = {"update"}
-    print("updating update script...")
     update_files(files)
     shell.run("update", "ready")
 end
